846428c926f2b9017ed238a44516d09c14dc06a2
Gabriel Anderson <145013468+gamakagami@users.noreply.github.com>
2024-09-26 21:04:54 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function buyNow() {
+    alert("Redirecting to the purchase page!");
+    // You can add actual logic to redirect the user to a product page.
+}
+
+function orderNow() {
+    alert("Order Now! Your product will be shipped soon.");
+    // Add actual logic to handle order placement.
+}
